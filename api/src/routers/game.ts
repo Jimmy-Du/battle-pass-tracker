7e5ff307cabb36b05bef90cc7f47595d419cf962
Jimmy-Du <du.jimmy@outlook.com>
2022-05-10 01:20:35 -0400

    Created /games route to retrieve games from database

---
+++ api/src/routers/game.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+import { QueryResult } from 'pg'
+import pool from '../db/db'
+const router: Application = express()
+
+
+
+// Route:       POST /games
+// Description: Gets all games with battle passes if no games are specified
+//              or the specified games if games are specified
+// Access:      Public
+router.post('/games', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // if no array or an empty array is passed in the request body, all games will be sent back
+    if (req.body.games === undefined || req.body.games.length === 0) {
+      const games: QueryResult = await pool.query("SELECT * FROM game")
+      res.status(200).send(games.rows)
+    }
+    // else, the specified games in the array are sent back
+    else {
+      const games: QueryResult = await pool.query("SELECT * FROM game WHERE id = ANY($1::int[])", [req.body.games])
+      res.status(200).send(games.rows)
+    }
+  }
+  catch {
+    res.status(500).send({error: "Something went wrong"})
+  }
+})
+
+
+
+export default router

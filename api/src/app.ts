2af6efe8dd6df237a2827a340b0168aac750e55a
Jimmy-Du <du.jimmy@outlook.com>
2022-05-18 17:03:02 -0400

    Added cors settings

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import gameRoute from './routers/game'
+const cors = require('cors')
 require('dotenv').config()
 
 
@@ -11,6 +12,9 @@ const app: express.Application = express()
 // Setup express options
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors({
+  origin: process.env.ORIGIN
+}))
 
 // Setup routes
 app.use(gameRoute)

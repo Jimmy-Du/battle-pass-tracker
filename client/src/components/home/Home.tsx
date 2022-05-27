12583a7be0ee7f0df47d78e4af0d14fe57457289
Jimmy-Du <du.jimmy@outlook.com>
2022-05-27 17:17:57 -0400

    Added functionality to get only selected games by the user

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -8,19 +8,32 @@ import './Home.css'
 const Home: React.FC = () => {
   const [games, setGames] = useState<Game[]>()
 
+
+
   // Function:    getGames()
   // Description: retrieves games with battle passes from database to display
   // Parameters:  N/A
   // Return:      N/A
   const getGames = async () => {
-    const response = await axios.post<Game[]>(`${process.env.REACT_APP_BASE_API_URL}/games`, {games: []})
+    let selectedGames: number[] = []
+
+    // if the user's preference of games is in local storage, the games will be retrieved
+    if (localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE!)) {
+      selectedGames = JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE!)!)
+    }
+
+    const response = await axios.post<Game[]>(`${process.env.REACT_APP_BASE_API_URL}/games`, {games: selectedGames})
     setGames(response.data)
   }
+
+
 
   // useEffect to make request to database to retrieve games when component is mounted
   useEffect(() => {
     getGames()
   }, [])
+
+
 
   return (
     <div className='home'>

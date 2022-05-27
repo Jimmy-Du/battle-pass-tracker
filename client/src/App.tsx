a6db2b49f6901f28da6a6b13a40a65aa17518c92
Jimmy-Du <du.jimmy@outlook.com>
2022-05-27 17:17:21 -0400

    Added select-games route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/navbar/Navbar';
 import Home from './components/home/Home';
+import SelectGames from './components/selectGames/SelectGames';
 import Footer from './components/layout/footer/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/select-games" element={<SelectGames />} />
       </Routes>
       <Footer />
     </div>

6bfc3dea23ab9b2bae98221c3e07cc44e4259221
Jimmy-Du <du.jimmy@outlook.com>
2022-05-29 16:09:30 -0400

    Added mobile functionality

--- client/src/components/layout/navbar/Navbar.tsx
+++ client/src/components/layout/navbar/Navbar.tsx
@@ -3,11 +3,26 @@ import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
 const Navbar = () => {
+  // Function:    toggleLinks()
+  // Description: called upon when the user clicks the show links button on mobile devices
+  //              and will display the navbar links
+  // Parameters:  e: info about the event that invoked the function
+  // Return:      N/A
+  const toggleLinks = (e: any) => {
+    const links = e.target.nextElementSibling
+    links.classList.toggle('active')
+  }
+
+
+
   return (
     <nav className='navbar'>
       <div className='brand-title'>
         Battle Pass Tracker
       </div>
+      <span className='toggle-btn' onClick={(e) => toggleLinks(e)}>
+        Menu
+      </span>
       <div className='navbar-links'>
         <ul>
           <li>

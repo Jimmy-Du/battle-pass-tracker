4af76ec6c2008b4fc09d74ace145dfcf4c3242d4
Jimmy-Du <du.jimmy@outlook.com>
2022-08-09 15:45:30 -0400

    Added events route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/layout/navbar/Navbar';
 import Games from './components/games/Games';
 import SelectGames from './components/selectGames/SelectGames';
 import Footer from './components/layout/footer/Footer';
+import Events from './components/events/Events';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Games />} />
+        <Route path="/events" element={<Events/>} />
         <Route path="/select-games" element={<SelectGames />} />
       </Routes>
       <Footer />

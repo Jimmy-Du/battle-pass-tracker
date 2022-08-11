a64f153c25ce2e1ba3ee03405db17028ea9c7703
Jimmy-Du <du.jimmy@outlook.com>
2022-08-11 17:22:37 -0400

    Added InfoDisplaySortMobile component to enable sorting for mobile users

---
+++ client/src/components/infoDisplay/InfoDisplayMobileSort/InfoDisplaySortMobile.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './InfoDisplaySortMobile.css'
+
+interface InfoDisplaySortMobileProps {
+  sortFunction: Function,
+  mainTitle: string,
+  subTitle: string
+}
+
+const InfoDisplaySortMobile: React.FC<InfoDisplaySortMobileProps> = ({ sortFunction, mainTitle, subTitle }) => {
+  return (
+    <div className='info-display-sort-mobile'>
+      <fieldset>
+        <legend>Sort by:</legend>
+          <select onChange={(e) => sortFunction(e.target.value)}>
+            <option value='main'>{ mainTitle }</option>
+            <option value='sub'>{ subTitle }</option>
+            <option value='start'>Start Date</option>
+            <option value='end'>End Date</option>
+            <option value='end'>Days Left</option>
+          </select>
+      </fieldset>
+    </div>
+  )
+}
+
+export default InfoDisplaySortMobile
